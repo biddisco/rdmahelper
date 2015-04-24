9afcefdb796823a8a68adf00006e7455becca05a
John Biddiscombe <biddisco@cscs.ch>
2015-04-24 09:54:22 +0200

    Thread safety in connection base, synchronize receive pop/push

--- include/RdmaConnectionBase.h
+++ include/RdmaConnectionBase.h
@@ -37,85 +37,88 @@ namespace bgcios {
 
 class RdmaConnectionBase {
 public:
-  // expected ops must be stored per client, so use a map with qp as key
-  typedef std::queue<struct na_verbs_op_id*> OperationQueue;
-  OperationQueue ExpectedOps;
-  std::queue<uint64_t> _waitingReceives;
+    // expected ops must be stored per client, so use a map with qp as key
+    typedef std::queue<struct na_verbs_op_id*> OperationQueue;
+    OperationQueue ExpectedOps;
+    std::queue<uint64_t> _waitingReceives;
 
-  //
-  RdmaConnectionBase() { }
+    //
+    RdmaConnectionBase() { }
 
-  virtual ~RdmaConnectionBase() {
-    while (!_waitingReceives.empty()) {
-      LOG_DEBUG_MSG("Connection closing, releasing region");
-      RdmaMemoryRegion *region = (RdmaMemoryRegion *)_waitingReceives.front();
-      this->releaseRegion(region);
-      _waitingReceives.pop();
+    virtual ~RdmaConnectionBase() {
+        while (!_waitingReceives.empty()) {
+            LOG_DEBUG_MSG("Connection closing, releasing region");
+            RdmaMemoryRegion *region = (RdmaMemoryRegion *)_waitingReceives.front();
+            this->releaseRegion(region);
+            _waitingReceives.pop();
+        }
+        // clear memory pool reference
+        LOG_DEBUG_MSG("releasing memory pool reference");
+        _memoryPool.reset();
     }
-    // clear memory pool reference
-    LOG_DEBUG_MSG("releasing memory pool reference");
-    _memoryPool.reset();
-  }
 
-  /*---------------------------------------------------------------------------*/
-  uint64_t popReceive() {
-    uint64_t wr_id = _waitingReceives.front();
-    this->_waitingReceives.pop();
-    LOG_DEBUG_MSG("After pop of " << hexpointer(wr_id) << " size of waiting receives is " << decnumber(_waitingReceives.size()))
-    return wr_id;
-  }
-
-  /*---------------------------------------------------------------------------*/
-  void pushReceive_(uint64_t wr_id) {
-    _waitingReceives.push(wr_id);
-    LOG_DEBUG_MSG("After push of " << hexpointer(wr_id) << "size of waiting receives is " << decnumber(_waitingReceives.size()))
-  }
-
-  /*---------------------------------------------------------------------------*/
-  //! The number of outstanding work requests
-  uint32_t getNumReceives() { return _waitingReceives.size(); }
-
-  /*---------------------------------------------------------------------------*/
-  void refill_preposts(unsigned int preposts) {
-    LOG_DEBUG_MSG("Entering refill size of waiting receives is " << decnumber(_waitingReceives.size()))
-    while (this->getNumReceives()<preposts) {
-      LOG_DEBUG_MSG("Pre-Posting a receive to client");
-      RdmaMemoryRegion *region = this->getFreeRegion(this->_memoryPool->default_chunk_size());
-      this->postRecvRegionAsID(region, region->getLength(), false);
+    /*---------------------------------------------------------------------------*/
+    uint64_t popReceive() {
+        std::lock_guard<std::mutex> lock(receive_mutex);
+        uint64_t wr_id = _waitingReceives.front();
+        this->_waitingReceives.pop();
+        LOG_DEBUG_MSG("After pop of " << hexpointer(wr_id) << " size of waiting receives is " << decnumber(_waitingReceives.size()))
+        return wr_id;
     }
-  }
 
-  /*---------------------------------------------------------------------------*/
-  void setMemoryPool(RdmaMemoryPoolPtr pool)
-  {
-    this->_memoryPool = pool;
-  }
-
-  /*---------------------------------------------------------------------------*/
-  RdmaMemoryRegion *getFreeRegion(size_t size)
-  {
-    FUNC_START_DEBUG_MSG;
-    RdmaMemoryRegion* region = this->_memoryPool->allocateRegion(size);
-    if (!region) {
-      LOG_ERROR_MSG("Error creating free memory region");
+    /*---------------------------------------------------------------------------*/
+    void pushReceive_(uint64_t wr_id) {
+        std::lock_guard<std::mutex> lock(receive_mutex);
+        _waitingReceives.push(wr_id);
+        LOG_DEBUG_MSG("After push of " << hexpointer(wr_id) << "size of waiting receives is " << decnumber(_waitingReceives.size()))
     }
-    region->setMessageLength(size);
 
-    return region;
-  }
+    /*---------------------------------------------------------------------------*/
+    //! The number of outstanding work requests
+    uint32_t getNumReceives() { return _waitingReceives.size(); }
 
-  /*---------------------------------------------------------------------------*/
-  void releaseRegion(RdmaMemoryRegion *region)
-  {
-   this->_memoryPool->deallocate(region);
-  }
+    /*---------------------------------------------------------------------------*/
+    void refill_preposts(unsigned int preposts) {
+        LOG_DEBUG_MSG("Entering refill size of waiting receives is " << decnumber(_waitingReceives.size()))
+            while (this->getNumReceives()<preposts) {
+                LOG_DEBUG_MSG("Pre-Posting a receive to client");
+                RdmaMemoryRegion *region = this->getFreeRegion(this->_memoryPool->default_chunk_size());
+                this->postRecvRegionAsID(region, region->getLength(), false);
+            }
+    }
 
-  /*---------------------------------------------------------------------------*/
-  virtual uint64_t
-  postRecvRegionAsID(RdmaMemoryRegion *region, uint32_t length, bool expected=false) = 0;
+    /*---------------------------------------------------------------------------*/
+    void setMemoryPool(RdmaMemoryPoolPtr pool)
+    {
+        this->_memoryPool = pool;
+    }
+
+    /*---------------------------------------------------------------------------*/
+    RdmaMemoryRegion *getFreeRegion(size_t size)
+    {
+        FUNC_START_DEBUG_MSG;
+        RdmaMemoryRegion* region = this->_memoryPool->allocateRegion(size);
+        if (!region) {
+            LOG_ERROR_MSG("Error creating free memory region");
+        }
+        region->setMessageLength(size);
+
+        return region;
+    }
+
+    /*---------------------------------------------------------------------------*/
+    void releaseRegion(RdmaMemoryRegion *region)
+    {
+        this->_memoryPool->deallocate(region);
+    }
+
+    /*---------------------------------------------------------------------------*/
+    virtual uint64_t
+    postRecvRegionAsID(RdmaMemoryRegion *region, uint32_t length, bool expected=false) = 0;
 
 protected:
-  RdmaMemoryPoolPtr _memoryPool;
+    RdmaMemoryPoolPtr _memoryPool;
+    std::mutex receive_mutex;
 
 };
 
